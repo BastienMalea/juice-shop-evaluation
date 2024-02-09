1dc0bbaacddf0a8ada3621144e6468834860bc17
BastienMalea <bastmalea@gmail.com>
2024-02-09 15:18:33 +0100

    Fix Injection SQL

--- routes/search.ts
+++ routes/search.ts
@@ -20,6 +20,12 @@ module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
+
+    // Je fais un replace assez strict pour avoir des recherches uniquement alpha numérique
+    // Dans burp on a un success mais on a data[] vide
+    // {"status":"success","data":[]}
+    criteria = criteria.replace(/[^a-zA-Z0-9 ]/g, '')
+
     models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
