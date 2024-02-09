a4421190481a9e69d77955c66f69d7c016d78640
BastienMalea <bastmalea@gmail.com>
2024-02-09 14:55:44 +0100

    Fix IDOR vulnerability

--- routes/basket.ts
+++ routes/basket.ts
@@ -15,17 +15,27 @@ const challenges = require('../data/datacache').challenges
 module.exports = function retrieveBasket () {
   return (req: Request, res: Response, next: NextFunction) => {
     const id = req.params.id
-    BasketModel.findOne({ where: { id }, include: [{ model: ProductModel, paranoid: false, as: 'Products' }] })
+    const user = security.authenticatedUsers.from(req)
+
+    if (!user || typeof user.data.id === 'undefined') {
+      return res.status(401).json({ error: 'You must be logged in to access this resource.' })
+    }
+
+    BasketModel.findOne({ where: { id, UserId: user.data.id }, include: [{ model: ProductModel, paranoid: false, as: 'Products' }] })
       .then((basket: BasketModel | null) => {
         /* jshint eqeqeq:false */
         challengeUtils.solveIf(challenges.basketAccessChallenge, () => {
           const user = security.authenticatedUsers.from(req)
           return user && id && id !== 'undefined' && id !== 'null' && id !== 'NaN' && user.bid && user.bid != id // eslint-disable-line eqeqeq
         })
-        if (((basket?.Products) != null) && basket.Products.length > 0) {
-          for (let i = 0; i < basket.Products.length; i++) {
-            basket.Products[i].name = req.__(basket.Products[i].name)
+        if (basket) {
+          if (basket.Products && basket.Products.length > 0) {
+            for (let i = 0; i < basket.Products.length; i++) {
+              basket.Products[i].name = req.__(basket.Products[i].name)
+            }
           }
+        } else {
+          res.status(404).send('Basket not found or access denied.')
         }
 
         res.json(utils.queryResultToJson(basket))
